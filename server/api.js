f3af1347f20af1cd3a1420e17d621bef618cc585
jagadeesh-debug <jaggurockz4892@gmail.com>
2025-06-22 09:26:48 +0530

    backend setup

--- server/api.js
+++ server/api.js
@@ -1,4 +1,13 @@
-const serverless = require("serverless-http");
-const app = require("./index");
 
-module.exports = serverless(app);
+// .then(() => {
+//     console.log("App connected to database");
+    
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+const app = require("./index");
+app.listen(8080, () => {
+        console.log(`listening:${8080}`);
+    });
+
